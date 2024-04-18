6110b283db0f175a76c885360ded2b7632d5df9b
cryptopig <vpig76@gmail.com>
2024-04-18 13:10:33 +0200

    t

--- script.py
+++ script.py
@@ -1,19 +1,39 @@
 from splinter import Browser
-from time import sleep
-browser = Browser('chrome')
-browser.visit('https://jumpstart.courseinstruction.com/course/content/page/userfront_login')
-print('connected to google.com')
 
-EMAIL = 'vedant.g273@gmail.com'
-PASSWORD = 'Vedu!1234'
+from time import sleep
+
+
+browser = Browser('chrome')
+print('successfully established browser')
+
+browser.visit('https://jumpstart.courseinstruction.com/course/content/page/userfront_login')
+print('opened portal')
+
+EMAIL = 'EMAIL'
+PASSWORD = 'PASSWORD'
 
 browser.find_by_id('email').click()
 browser.type('email', EMAIL)
 browser.find_by_id('password')
 browser.type('password', PASSWORD)
 
-browser.find_by_css('button medium green sign-in').click()
+browser.find_by_css('a[class="button medium green sign-in"]').click()
+sleep(1)
+
+# navigate to actual lesson, start loop from here
+
+browser.visit('https://jumpstart.courseinstruction.com/course/content/page/course_home')
+
+browser.find_by_id('navlink_outline').click()
+sleep(1)
+
+
+# while True:
+browser.find_by_css('a[href="javascript:if(true){GoTo(\'8\');}"').click()
+sleep(1)
+
+browser.find_by_css('a[href="javascript:if(true){GoTo(\'8.1\');}"').click()
+sleep(1)
 
 sleep(5)
-
 browser.quit()
